19e59aba99732626eab8e68d99e7ec1122221674
Shenandoah-Walker <saw67402@email.vccs.edu>
2025-06-26 21:16:00 +0000

    Created Interest-Earned branch

--- .ccls-cache/@home@runner@workspace/main.cpp
+++ .ccls-cache/@home@runner@workspace/main.cpp
@@ -1,4 +1,4 @@
-//Ingredient Adjuster Program
+//Interest Earned Program
 
 #include <iostream>
 #include <iomanip>
@@ -7,24 +7,5 @@ using namespace std;
 
 int main()
 {
-//Create variables for the original measurements for the original number of cookies.
-const double recipeSugar = 1.5, recipeButter = 1, recipeFlour = 2.75, recipeCookies = 48;
-  
-/*Create variables for the new measurements for the new number of cookies and initialize the number of cookies entered by the user.*/
-double newSugar, newButter, newFlour, newCookies;
 
-//Prompt the user to input the value for newCookies, the number of cookies the user wants to make.
-cout << "How many cookies would you like to make? ";
-cin >> newCookies;
-
-//Calculate the new measurements for the new number of cookies.
-newSugar = (recipeSugar / recipeCookies) * newCookies;
-newButter = (recipeButter / recipeCookies) * newCookies;
-newFlour = (recipeFlour / recipeCookies) * newCookies;
-
-//Print the new measurements for the new number of cookies to the console.
-cout << "To make " << newCookies << " cookie(s), you will need: " << endl;
-cout << newSugar << " cup(s) of sugar" << endl;
-cout << newButter << " cup(s) of butter" << endl;
-cout << newFlour << " cup(s) of flour" << endl;
 }
