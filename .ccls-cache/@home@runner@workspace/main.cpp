2b6bb5661e757c474e51403e451a0fbf7141ba23
Shenandoah-Walker <saw67402@email.vccs.edu>
2025-06-27 18:22:57 +0000

    Began writing code

--- .ccls-cache/@home@runner@workspace/main.cpp
+++ .ccls-cache/@home@runner@workspace/main.cpp
@@ -2,10 +2,20 @@
 
 #include <iostream>
 #include <iomanip>
+#include <cmath>
 
 using namespace std;
 
 int main()
 {
+double principal, rate, T, amount;
+    cout << "Enter the principal: ";
+    cin >> principal;
+    cout << "Enter the interest rate: ";
+    cin >> rate;
+    cout << "Enter the number of times the interest is compounded (T is 4 if the interest is compounded quarterly): ";
+    cin >> T;
 
+   amount = principal * pow((1 + (rate / T)), (T * 1));
+  cout << amount << endl;
 }
