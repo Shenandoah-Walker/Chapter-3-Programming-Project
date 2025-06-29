1e8ed77829c39237508177a7fdb3ff727cad5e28
Shenandoah-Walker <saw67402@email.vccs.edu>
2025-06-29 18:39:21 +0000

    Fixed output style

--- main.cpp
+++ main.cpp
@@ -21,9 +21,14 @@ int main()
 //Calculate the correct answer 
   correctAnswer = number1 + number2;
 
-//Display the two random numbers and prompt the user to calculate the correct answer
-  cout << "Calculate " << number1 << " + " << number2 << ". Then, press the Enter key when you are ready to view the correct answer." << endl;
-//Display the correct answer after the user presses the Enter key
+//Display the two random numbers as an addition problem and prompt the user to calculate the correct answer
+    cout << "Solve the addition problem below. Then, press the Enter key when you are ready to view the correct answer." << endl;
+    cout << endl;
+    cout << setw(5) << number1 << endl;
+    cout << "+ " << setw(3) << number2 << endl;
+    cout << "-----" << endl;
+    
+//Display the correct answer below the line after the user presses the Enter key
   cin.get();
-  cout << "The correct answer is: " << correctAnswer << endl;
+  cout << setw(5) << correctAnswer << endl;
 }
