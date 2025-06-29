dd06e7f5c7386c2d76115e7daacfdb2b73de8a2e
Shenandoah-Walker <saw67402@email.vccs.edu>
2025-06-29 17:36:01 +0000

    Added comments

--- .ccls-cache/@home@runner@workspace/main.cpp
+++ .ccls-cache/@home@runner@workspace/main.cpp
@@ -8,6 +8,7 @@ using namespace std;
 
 int main()
 {
+/* Initialize variables: principal is the balance in the savings account, rate is the interest rate, T is the number of times the interest is compounded per year, and amount is the amount of money accumulated after one year, including interest.The values of these variables are inputted by the user.*/
 double principal, rate, T, amount;
     cout << "Enter the principal: ";
     cin >> principal;
@@ -16,7 +17,10 @@ double principal, rate, T, amount;
     cout << "Enter the number of times the interest is compounded (T is 4 if the interest is compounded quarterly): ";
     cin >> T;
 
+    //Calculate the amount of money accumulated after one year, including interest.
    amount = principal * pow((1 + (rate / T)), (T * 1));
+
+    //Output the results.
     cout << "Interest Rate: $" << rate << endl;
     cout << "Times Compounded: " << T << endl;
     cout << "Principal: $" << principal << endl;
