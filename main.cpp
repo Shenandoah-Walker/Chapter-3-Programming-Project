a337f3358b5c7c532fa50d683673e6322b6635a8
Shenandoah-Walker <saw67402@email.vccs.edu>
2025-06-29 18:28:20 +0000

    Changed output design

--- main.cpp
+++ main.cpp
@@ -10,21 +10,23 @@ int main()
 {
 /* Initialize variables: principal is the balance in the savings account, rate is the interest rate, T is the number of times the interest is compounded per year, and amount is the amount of money accumulated after one year, including interest.The values of these variables are inputted by the user.*/
 double principal, rate, T, amount;
-    cout << "Enter the principal: ";
+    cout << "Enter the principal: $ ";
     cin >> principal;
-    cout << "Enter the interest rate: ";
+    cout << "Enter the interest rate (do not include the % symbol): ";
     cin >> rate;
     cout << "Enter the number of times the interest is compounded (T is 4 if the interest is compounded quarterly): ";
     cin >> T;
 
     //Calculate the amount of money accumulated after one year, including interest.
-   amount = principal * pow((1 + (rate / T)), (T * 1));
+   amount = principal * pow((1 + ((rate / 100) / T)), (T * 1));
 
-    //Output the results.
-    cout << "Interest Rate: $" << rate << endl;
-    cout << "Times Compounded: " << T << endl;
-    cout << "Principal: $" << principal << endl;
-    cout << "Interest: $" << amount - principal << endl;
-    cout << "Amount in Savings: $" << amount << endl;
+
+    //Output the results, with all variables except T having two decimal places.
+    cout << fixed << setprecision(2);
+    cout << "Interest Rate: " << setw(13) << rate << "%" << endl;
+    cout << "Times Compounded: " << setprecision(0) << setw(10) << T << endl;
+    cout << "Principal:" << setprecision(2) << setw(11) << "$ " << principal << endl;
+    cout << "Interest:" << setw(12) << "$ " << setw(7) << amount - principal << endl;
+    cout << "Amount in Savings: $ " << amount << endl;
     
 }
